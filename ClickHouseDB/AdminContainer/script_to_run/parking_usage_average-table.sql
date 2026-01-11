60217bb7a9278161af99dd0fa3993c30250b0e1d
Benjamin Jakupovic <bjakupo@gmail.com>
2026-01-11 10:57:24 +0100

    update parking_usage_baseline model

--- ClickHouseDB/AdminContainer/script_to_run/parking_usage_average-table.sql
+++ ClickHouseDB/AdminContainer/script_to_run/parking_usage_average-table.sql
@@ -2,7 +2,6 @@ CREATE TABLE parking_db.parking_usage_baseline (
     baseline_id String,          -- Unique ID for this weekly calculation run
     calculation_date Date,       -- The date this average was computed
     parking_lot_id String,       -- Reference to the lot
-    day_of_week UInt8,           -- 1 (Monday) to 7 (Sunday)
     hour_of_day UInt8,           -- 0 to 23
     
     avg_car_count Float64,       -- The "mean" (mu)
@@ -12,4 +11,4 @@ CREATE TABLE parking_db.parking_usage_baseline (
     
     is_active UInt8 DEFAULT 1    -- Flag to indicate if this is the "current" baseline
 ) ENGINE = ReplacingMergeTree()
-ORDER BY (parking_lot_id, day_of_week, hour_of_day, calculation_date);
+ORDER BY (parking_lot_id, hour_of_day, calculation_date);
